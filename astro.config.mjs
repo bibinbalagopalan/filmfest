013c03dfdf2164bb7594b96faff9ebd7ab35043f
Bibin M <110320521+bibinbalagopalan@users.noreply.github.com>
2026-08-27 17:30:32 +0530

    Commit post Supabase integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
+// astro.config.mjs
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
