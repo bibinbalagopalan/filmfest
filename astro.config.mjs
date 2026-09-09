31583a9c3cd3051c29dc2a392b5c47fdde97aebc
Bibin M <110320521+bibinbalagopalan@users.noreply.github.com>
2026-09-09 17:36:49 +0530

    fix: removed cloudflare adapter to use static pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static', // Change this to 'server' or 'hybrid' if you use SSR
-  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
